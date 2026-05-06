a34da7f1c62fc11099d764f1d251060e9e5193d4
lishamoni99 <23201099@uap-bd.edu>
2026-05-06 22:23:04 +0600

    Commit by Tabassum Siddika Lishamoni

---
+++ smartqueue/queue_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+
+    path('student-login/', views.student_login, name='student_login'),
+    path('sector-select/', views.sector_select, name='sector_select'),
+    path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
+
+    path('guardian-login/', views.guardian_login, name='guardian_login'),
+    path('guardian-dashboard/', views.guardian_dashboard, name='guardian_dashboard'),
+
+    path('logout/', views.logout_view, name='logout'),
+]
