3c44192914aba2f42fb698e2b7fe05ac959dca01
nabila <23201093@uap-bd.edu>
2026-05-09 12:44:11 +0600

    inbox.html

--- smartqueue/notifications/models.py
+++ smartqueue/notifications/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Notification(models.Model):
     student_id = models.CharField(max_length=100)
     message = models.TextField()
--- smartqueue/notifications/views.py
+++ smartqueue/notifications/views.py
@@ -6,13 +6,13 @@ def inbox(request):
     student_id = request.session.get('student_id')
 
     if not student_id:
-        return render(request, 'notifications/inbox.html', {'error': 'Session expired. Please login again.'})
+        return render(request, 'inbox.html', {'error': 'Session expired. Please login again.'})
 
 
     notifications = Notification.objects.filter(student_id=student_id).order_by('-created_at')
 
     Notification.objects.filter(student_id=student_id, is_read=False).update(is_read=True)
 
-    return render(request, 'notifications/inbox.html', {
+    return render(request, 'inbox.html', {
         'notifications': notifications
     })
