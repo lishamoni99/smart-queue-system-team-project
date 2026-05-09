4be946eca9684d081ac79ee2d0e6904cfd3a7159
nabila <23201093@uap-bd.edu>
2026-05-09 12:25:55 +0600

    inbox

--- smartqueue/notifications/views.py
+++ smartqueue/notifications/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Notification
 
-# Create your views here.
+
+def inbox(request):
+    student_id = request.session.get('student_id')
+
+    if not student_id:
+        return render(request, 'notifications/inbox.html', {'error': 'Session expired. Please login again.'})
+
+
+    notifications = Notification.objects.filter(student_id=student_id).order_by('-created_at')
+
+    Notification.objects.filter(student_id=student_id, is_read=False).update(is_read=True)
+
+    return render(request, 'notifications/inbox.html', {
+        'notifications': notifications
+    })
