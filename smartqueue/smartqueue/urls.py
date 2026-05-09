1f6e0521a9e65f5c60a970895f726d9bf82f69ac
nabila <23201093@uap-bd.edu>
2026-05-09 12:59:35 +0600

    inbox include

--- smartqueue/smartqueue/urls.py
+++ smartqueue/smartqueue/urls.py
@@ -14,34 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from django.conf import settings
-from django.conf.urls.static import static
 from feedback.views import feedback_view
-=======
 from django.shortcuts import redirect
 
 def home(request):
     return redirect('student_login')
->>>>>>> 6a7a41b34536667b9af969cbe9c39fb09d7988d1
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('', include('queue_app.urls')),
-<<<<<<< HEAD
     path('feedback/', feedback_view, name='feedback'),
-]
+    path('history/', include('history.urls')),
+    path('notifications/', include('notifications.urls')),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 ]
-
-path('history/', include('history.urls')),
->>>>>>> 6a7a41b34536667b9af969cbe9c39fb09d7988d1
