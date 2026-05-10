969655e394560b105cabd797dcd26b0bf54e4fef
nabila <23201093@uap-bd.edu>
2026-05-11 03:33:52 +0600

    faq view

--- smartqueue/faq_app/views.py
+++ smartqueue/faq_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import FAQ
 
-# Create your views here.
+def faq_list(request):
+    faqs = FAQ.objects.all().order_by('-created_at')
+    return render(request, 'faq_app/faq.html', {'faqs': faqs})
